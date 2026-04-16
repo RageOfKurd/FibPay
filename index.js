e0dae514692e8586faf366a11e21319cfbfd8e26
rageofkurd <rageofkurd98@gmail.com>
2026-04-16 10:12:18 +0300

    feat: add ESM compatibility (support import FibPay from 'fibpay')

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ const { FibPayError } = require('./src/payments');
 module.exports = FibPay;
 module.exports.FibPay = FibPay;
 module.exports.FibPayError = FibPayError;
+module.exports.default = FibPay;
